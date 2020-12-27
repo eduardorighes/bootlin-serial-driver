4e625f3dfc70c13d2c00bea2c4418907d5886c9e
Eduardo Righes <eduardo.righes@gmail.com>
2020-12-27 17:42:25 +0000

    Updated for tx, rx counters.
    Signed-off-by: Eduardo Righes <eduardo.righes@gmail.com>

--- serial-get-counter.c
+++ serial-get-counter.c
@@ -4,32 +4,45 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define SERIAL_RESET_COUNTER 0
-#define SERIAL_GET_COUNTER 1
+#define SERIAL_GET_TX_COUNTER 1
+#define SERIAL_GET_RX_COUNTER 2
 
 int main(int argc, char *argv[])
 {
-    unsigned int val;
-    int fd, ret;
+	unsigned int tx = 0, rx = 0;
+	int fd, ret;
 
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s /dev/UART\n", argv[0]);
-	exit (1);
-    }
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s /dev/UART\n", argv[0]);
+		exit (1);
+	}
 
-    fd = open(argv[1], O_RDWR);
-    if (fd < 0) {
-	fprintf(stderr, "Unable to open %s\n", argv[1]);
-        exit (1);
-    }
+	fd = open(argv[1], O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "Unable to open %s\n", argv[1]);
+		exit (1);
+	}
 
-    ret = ioctl(fd, SERIAL_GET_COUNTER, & val);
-    if (ret < 0) {
-        fprintf(stderr, "Unable to get counter\n");
-        exit (1);
-    }
+	ret = ioctl(fd, SERIAL_GET_TX_COUNTER, &tx);
+	if (ret < 0) {
+		fprintf(stderr, "Unable to get tx counter\n");
+		exit (1);
+	} else {
+		printf("tx counter: %d\n", tx);
+	}
 
-    printf("Counter value: %d\n", val);
-    return 0;
+	ret = ioctl(fd, SERIAL_GET_RX_COUNTER, &rx);
+	if (ret < 0) {
+		fprintf(stderr, "Unable to get rx counter\n");
+		exit (1);
+	} else {
+		printf("rx counter: %d\n", rx);
+	}
+	
+	close(fd);
+
+	return 0;
 }
